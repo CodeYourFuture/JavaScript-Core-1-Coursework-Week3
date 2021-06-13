fc4fb74488672ee8c6a4516e9643c08e6e35d5c7
Daniel Wagner-Hall <dawagner@gmail.com>
2021-06-13 17:50:57 +0100

    Add note that exercise is intentional repeat

--- 2-mandatory/1-create-functions.js
+++ 2-mandatory/1-create-functions.js
@@ -15,6 +15,9 @@ function sortArray() {
 }
 
 /*
+NOTE: This exercise is the same as one you did last week - try to do it again using things you learnt this week.
+Think about what is better about this solution than your one last week, and what is worse.
+
 Write a function that:
 - Takes an array of strings as input.
 - Removes any spaces in the beginning or end each string.
