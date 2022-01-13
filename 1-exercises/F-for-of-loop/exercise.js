46f3ac03dafa216a1cfccd3fa666906f3288caad
Saman Zahedi <saman.zahedi10@gmail.com>
2022-01-13 20:25:10 +0000

    mandatory done

--- 1-exercises/F-for-of-loop/exercise.js
+++ 1-exercises/F-for-of-loop/exercise.js
@@ -1,16 +1,23 @@
 /*
-    A for-of loop is a easy and way of looping through the elements of an array, string or any other "iterable object" (think sequence of elements).    
+    A for-of loop is a easy way of looping through the elements of an array, string or 
+    any other "iterable object" (think sequence of elements).    
 */
 
 // TODO Use a for-of loop to output each of the tube stations below.
 let tubeStations = [
-    "Aldgate",
-    "Baker Street",
-    "Picadilly Circus",
-    "Oxford Street",
-    "Tottenham Court Road"
+  "Aldgate",
+  "Baker Street",
+  "Picadilly Circus",
+  "Oxford Street",
+  "Tottenham Court Road",
 ];
 
+for (let i = 0; i < tubeStations.length; i++) {
+  console.log(tubeStations[i]);
+}
 
 // TODO Use a for-of loop to capitalise and output each letter in the string seperately.
 let str = "codeyourfuture";
+for (let letter of str) {
+  console.log(letter.toUpperCase());
+}
--- 2-mandatory/4-stocks.js
+++ 2-mandatory/4-stocks.js
@@ -6,18 +6,16 @@
         - an array of arrays containing the closing price for each stock in each of the last 5 days.
             For example, CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS[2] contains the prices for the last 5 days for STOCKS[2] (which is amzn)
 */
-
 /* ======= Stock data - DO NOT MODIFY ===== */
 const STOCKS = ["aapl", "msft", "amzn", "googl", "tsla"];
 
 const CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS = [
-    [179.19, 180.33, 176.28, 175.64, 172.99], // AAPL
-    [340.69, 342.45, 334.69, 333.20, 327.29], // MSFT
-    [3384.44, 3393.39, 3421.37, 3420.74, 3408.34], // AMZN
-    [2951.88, 2958.13, 2938.33, 2928.30, 2869.45], // GOOGL
-    [1101.30, 1093.94, 1067.00, 1008.87, 938.53] // TSLA
+  [179.19, 180.33, 176.28, 175.64, 172.99], // AAPL
+  [340.69, 342.45, 334.69, 333.2, 327.29], // MSFT
+  [3384.44, 3393.39, 3421.37, 3420.74, 3408.34], // AMZN
+  [2951.88, 2958.13, 2938.33, 2928.3, 2869.45], // GOOGL
+  [1101.3, 1093.94, 1067.0, 1008.87, 938.53], // TSLA
 ];
-
 /*
     We want to understand what the average price over the last 5 days for each stock is.
     Implement the below function, which
@@ -34,7 +32,19 @@ const CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS = [
         Functions can help with this!
 */
 function getAveragePrices(closingPricesForAllStocks) {
-    // TODO
+  // TODO
+  let stockPriceAverage = [];
+  for (let i = 0; i < closingPricesForAllStocks.length; i++) {
+    let stockPriceTotal = 0;
+    let stockPrice = closingPricesForAllStocks[i];
+    for (let j = 0; j < stockPrice.length; j++) {
+      stockPriceTotal += stockPrice[j];
+    }
+    stockPriceAverage.push(
+      parseFloat((stockPriceTotal / stockPrice.length).toFixed(2))
+    );
+  }
+  return stockPriceAverage;
 }
 
 /*
@@ -48,7 +58,15 @@ function getAveragePrices(closingPricesForAllStocks) {
     The price change value should be rounded to 2 decimal places, and should be a number (not a string)
 */
 function getPriceChanges(closingPricesForAllStocks) {
-    // TODO
+  // TODO
+  changedPrice = [];
+  for (const price of closingPricesForAllStocks) {
+    let priceDifference = parseFloat(
+      (price[price.length - 1] - price[0]).toFixed(2)
+    );
+    changedPrice.push(priceDifference);
+  }
+  return changedPrice;
 }
 
 /*
@@ -64,31 +82,41 @@ function getPriceChanges(closingPricesForAllStocks) {
     The price should be shown with exactly 2 decimal places.
 */
 function highestPriceDescriptions(closingPricesForAllStocks, stocks) {
-    // TODO
+  // TODO
+  let highestPriceForEachStock = [];
+  for (let i = 0; i < closingPricesForAllStocks.length; i++) {
+    highestPriceForEachStock.push(
+      `The highest price of ${stocks[
+        i
+      ].toUpperCase()} in the last 5 days was ${Math.max(
+        ...closingPricesForAllStocks[i]
+      ).toFixed(2)}`
+    );
+  }
+  return highestPriceForEachStock;
 }
-
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 test("should return the average price for each stock", () => {
-    expect(getAveragePrices(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual(
-        [176.89, 335.66, 3405.66, 2929.22, 1041.93]
-    );
+  expect(getAveragePrices(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual([
+    176.89, 335.66, 3405.66, 2929.22, 1041.93,
+  ]);
 });
 
 test("should return the price change for each stock", () => {
-    expect(getPriceChanges(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual(
-        [-6.2, -13.4, 23.9, -82.43, -162.77]
-    );
+  expect(getPriceChanges(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual([
+    -6.2, -13.4, 23.9, -82.43, -162.77,
+  ]);
 });
 
 test("should return a description of the highest price for each stock", () => {
-    expect(highestPriceDescriptions(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS, STOCKS)).toEqual(
-        [
-            "The highest price of AAPL in the last 5 days was 180.33",
-            "The highest price of MSFT in the last 5 days was 342.45",
-            "The highest price of AMZN in the last 5 days was 3421.37",
-            "The highest price of GOOGL in the last 5 days was 2958.13",
-            "The highest price of TSLA in the last 5 days was 1101.30"
-        ]
-    );
+  expect(
+    highestPriceDescriptions(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS, STOCKS)
+  ).toEqual([
+    "The highest price of AAPL in the last 5 days was 180.33",
+    "The highest price of MSFT in the last 5 days was 342.45",
+    "The highest price of AMZN in the last 5 days was 3421.37",
+    "The highest price of GOOGL in the last 5 days was 2958.13",
+    "The highest price of TSLA in the last 5 days was 1101.30",
+  ]);
 });
