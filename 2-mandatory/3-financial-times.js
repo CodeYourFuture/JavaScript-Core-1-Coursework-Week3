3ba12130f85e137409871209d8a2c16f40f4f846
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-01-14 16:03:41 +0000

    London Class 8-Davood Khoshnood-JS-Core 1-Week 3
    Last commit!

--- 2-mandatory/3-financial-times.js
+++ 2-mandatory/3-financial-times.js
@@ -16,7 +16,8 @@ function potentialHeadlines(allArticleTitles) {
 function titleWithFewestWords(allArticleTitles) {
   let result = allArticleTitles[0]
   for (let i = 0; i < allArticleTitles.length; i++) {
-    if (allArticleTitles[i].length < result.length) result = allArticleTitles[i]
+    if (allArticleTitles[i].split(' ').length < result.split(' ').length)
+      result = allArticleTitles[i]
   }
   return result
 }
@@ -43,18 +44,6 @@ function averageNumberOfCharacters(allArticleTitles) {
 }
 
 /* ======= List of Articles - DO NOT MODIFY ===== */
-const ARTICLE_TITLES = [
-  'Streaming wars drive media groups to spend more than $100bn on new content',
-  'Amazon Prime Video India country head: streaming is driving a TV revolution',
-  'Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights',
-  'British companies look to muscle in on US retail investing boom',
-  "Libor to take firm step towards oblivion on New Year's Day",
-  'Audit profession unattractive to new recruits, says PwC boss',
-  'Chinese social media users blast Elon Musk over near miss in space',
-  "Companies raise over $12tn in 'blockbuster' year for global capital markets",
-  'The three questions that dominate investment',
-  "Brussels urges Chile's incoming president to endorse EU trade deal",
-]
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
