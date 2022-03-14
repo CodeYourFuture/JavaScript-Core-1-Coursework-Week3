4a6efbcca15c50c78128ae681ea4769497f6cce6
SenamiAhifon <89362897+SenamiAhifon@users.noreply.github.com>
2022-03-14 21:34:10 +0000

    Exercises

--- 1-exercises/E-for-loop/exercise2.js
+++ 1-exercises/E-for-loop/exercise2.js
@@ -28,6 +28,10 @@ const AGES = [
 
 // TODO - Write for loop code here
 
+for (let i = 0; i < WRITERS.length; i++) {
+  console.log(`${WRITERS[i]} is ${AGES[i]} years old`);
+}
+
 /*
 The output should look something like this:
 
