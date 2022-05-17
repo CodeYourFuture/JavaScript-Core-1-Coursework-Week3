03de79c6a659d678d48c12e84a82112814563867
Alamin-eng <alaminruben@gmail.com>
2022-05-17 02:12:46 +0100

    full stretch

--- 3-extra/2-array-of-objects.js
+++ 3-extra/2-array-of-objects.js
@@ -9,132 +9,85 @@
     Implement a function which takes the array of books as a parameter, and returns an array of book titles.
     Each title in the resulting array should be the highest rated book in its genre.
 */
-const BOOKS = [
-  {
-    title: "The Lion, the Witch and the Wardrobe",
-    genre: "children",
-    rating: 4.7,
-  },
-  {
-    title: "Sapiens: A Brief History of Humankind",
-    genre: "non-fiction",
-    rating: 4.7,
-  },
-  {
-    title: "Nadiya's Fast Flavours",
-    genre: "cooking",
-    rating: 4.7,
-  },
-  {
-    title: "Harry Potter and the Philosopher's Stone",
-    genre: "children",
-    rating: 4.8,
-  },
-  {
-    title: "A Life on Our Planet",
-    genre: "non-fiction",
-    rating: 4.8,
-  },
-  {
-    title:
-      "Dishoom: The first ever cookbook from the much-loved Indian restaurant",
-    genre: "cooking",
-    rating: 4.85,
-  },
-  {
-    title: "Gangsta Granny Strikes Again!",
-    genre: "children",
-    rating: 4.9,
-  },
-  {
-    title: "Diary of a Wimpy Kid",
-    genre: "children",
-    rating: 4.6,
-  },
-  {
-    title: "BOSH!: Simple recipes. Unbelievable results. All plants.",
-    genre: "cooking",
-    rating: 4.6,
-  },
-  {
-    title: "The Book Your Dog Wishes You Would Read",
-    genre: "non-fiction",
-    rating: 4.85,
-  },
-];
-function getHighestRatedInEachGenre(books) {  // This problem is not solved yet as I have only output only the highest rated books object
-    let highestRated = {}
-    for(let book of books){
-        highestRated = book.rating
 
-        console.log(highestRated) // need fix it 
-    }
-   
-} // This problem is not solved yet as  I have only output only the highest rated books object
-getHighestRatedInEachGenre(BOOKS)
+function getHighestRatedInEachGenre(books) {  // This problem is not solved yet as I have only output only the highest rated books object
+   let sort = books.sort((a,b) => {
+     if(a.rating> b.rating){
+       return -1
+     } else if (b.rating > a.rating){
+       return 1
+     } else { return 0} 
+
+   })
+   let list = []
+   for(let i = 0 ; i < sort.length; i++){
+     list.push(sort[i].title)
+   }
+   return list.slice(0, 3)
+} 
 
 /* ======= Book data - DO NOT MODIFY ===== */
-// const BOOKS = [
-//     {
-//         title: "The Lion, the Witch and the Wardrobe",
-//         genre: "children",
-//         rating: 4.7
-//     },
-//     {
-//         title: "Sapiens: A Brief History of Humankind",
-//         genre: "non-fiction",
-//         rating: 4.7
-//     },
-//     {
-//         title: "Nadiya's Fast Flavours",
-//         genre: "cooking",
-//         rating: 4.7
-//     },
-//     {
-//         title: "Harry Potter and the Philosopher's Stone",
-//         genre: "children",
-//         rating: 4.8
-//     },
-//     {
-//         title: "A Life on Our Planet",
-//         genre: "non-fiction",
-//         rating: 4.8
-//     },
-//     {
-//         title: "Dishoom: The first ever cookbook from the much-loved Indian restaurant",
-//         genre: "cooking",
-//         rating: 4.85
-//     },
-//     {
-//         title: "Gangsta Granny Strikes Again!",
-//         genre: "children",
-//         rating: 4.9
-//     },
-//     {
-//         title: "Diary of a Wimpy Kid",
-//         genre: "children",
-//         rating: 4.6
-//     },
-//     {
-//         title: "BOSH!: Simple recipes. Unbelievable results. All plants.",
-//         genre: "cooking",
-//         rating: 4.6
-//     },
-//     {
-//         title: "The Book Your Dog Wishes You Would Read",
-//         genre: "non-fiction",
-//         rating: 4.85
-//     },
-// ]
+const BOOKS = [
+    {
+        title: "The Lion, the Witch and the Wardrobe",
+        genre: "children",
+        rating: 4.7
+    },
+    {
+        title: "Sapiens: A Brief History of Humankind",
+        genre: "non-fiction",
+        rating: 4.7
+    },
+    {
+        title: "Nadiya's Fast Flavours",
+        genre: "cooking",
+        rating: 4.7
+    },
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        genre: "children",
+        rating: 4.8
+    },
+    {
+        title: "A Life on Our Planet",
+        genre: "non-fiction",
+        rating: 4.8
+    },
+    {
+        title: "Dishoom: The first ever cookbook from the much-loved Indian restaurant",
+        genre: "cooking",
+        rating: 4.85
+    },
+    {
+        title: "Gangsta Granny Strikes Again!",
+        genre: "children",
+        rating: 4.9
+    },
+    {
+        title: "Diary of a Wimpy Kid",
+        genre: "children",
+        rating: 4.6
+    },
+    {
+        title: "BOSH!: Simple recipes. Unbelievable results. All plants.",
+        genre: "cooking",
+        rating: 4.6
+    },
+    {
+        title: "The Book Your Dog Wishes You Would Read",
+        genre: "non-fiction",
+        rating: 4.85
+    },
+]
 
 
 /* ======= TESTS - DO NOT MODIFY ===== */
-// test("should return the highest rated book in each genre", () => {
-//     expect(new Set(getHighestRatedInEachGenre(BOOKS))).toEqual(new Set(
-//         [
-//             "The Book Your Dog Wishes You Would Read",
-//             "Gangsta Granny Strikes Again!",
-//             "Dishoom: The first ever cookbook from the much-loved Indian restaurant"
-//         ]
-//     ));
-// });
+test("should return the highest rated book in each genre", () => {
+    expect(new Set(getHighestRatedInEachGenre(BOOKS))).toEqual(new Set(
+        [
+            "The Book Your Dog Wishes You Would Read",
+            "Gangsta Granny Strikes Again!",
+            "Dishoom: The first ever cookbook from the much-loved Indian restaurant"
+        ]
+    ));
+});
