2809a9ee3b6e52e975c10b74b9ae8b9e30da7214
mathias02 <matthiasm2016@gmail.com>
2022-07-11 23:06:41 +0200

    add fix

--- 1-exercises/B-while-loop/exercise.js
+++ 1-exercises/B-while-loop/exercise.js
@@ -13,18 +13,17 @@ function evenNumbers(n) {
     while(i < n){
     
         let num = n[i];
-        if(num !== 0){
-            
-        }
-    const j = doing.push(num);
+        if(num !== 0 && num % 2 === 0) return doing.push(num);
+    
         i++
     }
 
-    return j;
 }
+
 const j =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 evenNumbers(j);
+console.log(evenNumbers(j));
 
 evenNumbers(3); // should output 0,2,4
 evenNumbers(0); // should output nothing
