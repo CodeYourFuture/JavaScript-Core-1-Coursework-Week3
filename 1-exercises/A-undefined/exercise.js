eb5af7d52b4af340f8ba7e242dde46db4f06c108
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-07-14 00:30:48 +0200

    complete exercises A and B

--- 1-exercises/A-undefined/exercise.js
+++ 1-exercises/A-undefined/exercise.js
@@ -11,26 +11,23 @@
 
 // Example 1
 let a;
-console.log(a);
-
+console.log(a); // A VARIABLE HAS BEEN DEFINED BUT NOT ASSIGNED A VALUE, THEREFORE IT WILL RETURN AN UNDEFINED VALUE WHEN ACCESSING THE VARIABLE.
 
 // Example 2
 function sayHello() {
-    let message = "Hello";
+  let message = "Hello"; // A FUNCTION WITH THAT DOES NOT HAVE A RETURN WILL COME BACK AS UNDEFINED.
 }
 
-let hello = sayHello();
-console.log(hello);
-
+let hello = sayHello(); // sayHello IN THIS CASE IS NOT A COMPLETED FUNCTION SO WILL DO NOTHING WHEN CALLED UPON.
+console.log(hello); // THE OUTPUT OF WHICH, GIVES AN UNDEFINED MESSAGE.
 
 // Example 3
 function sayHelloToUser(user) {
-    console.log(`Hello ${user}`);
+  console.log(`Hello ${user}`); // THE PARAMETER REQUIRES AN ARGUMENT WHEN THE FUNCTION IS CALLED. ARGUMENT IN THIS CASE IS BLANK SO THE OUTPUT IS UNDEFINED.
 }
 
 sayHelloToUser();
 
-
 // Example 4
-let arr = [1,2,3];
-console.log(arr[3]);
+let arr = [1, 2, 3]; // THIS ARRAY CONSISTS OF ONLY 3 NUMBERS WITH AN INDEX THAT STARTS AT ZERO, SO THE HIGHEST NUMBER IS 2
+console.log(arr[3]); // AND THE USER IS TRYING TO ACCESS A FOURTH NUMBER WHICH WOULD HAVE HAD AN INDEX NUMBER OF 3 WERE IT
