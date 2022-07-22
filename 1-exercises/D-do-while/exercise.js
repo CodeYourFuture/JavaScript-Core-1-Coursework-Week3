e20986a8174b9d56b3dfd172cd523ca76809a32b
Chezlin Benson <chezlinbenson11@gmail.com>
2022-07-22 16:33:18 +0200

    Completed exercise D

--- 1-exercises/D-do-while/exercise.js
+++ 1-exercises/D-do-while/exercise.js
@@ -8,6 +8,14 @@
 
 function evenNumbersSum(n) {
     // TODO
+    let i = 0;
+    let sum = 0;
+    
+    do {
+       sum = sum + i * 2;       
+       i++; 
+    } while (i < n);
+    return sum;
 }
 
 console.log(evenNumbersSum(3)); // should output 6
