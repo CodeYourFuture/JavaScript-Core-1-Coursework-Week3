e43718a625a4e4a807d7115d6f6585b302ea4e4e
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-08-15 22:22:58 +0100

    Solution C

--- 1-exercises/C-while-loop-with-array/exercise.js
+++ 1-exercises/C-while-loop-with-array/exercise.js
@@ -5,19 +5,25 @@
 */
 
 const BIRTHDAYS = [
-    "January 7th",
-    "February 12th",
-    "April 3rd",
-    "April 5th",
-    "May 3rd",
-    "July 11th",
-    "July 17th",
-    "September 28th",
-    "November 15th"
+  "January 7th",
+  "February 12th",
+  "April 3rd",
+  "April 5th",
+  "May 3rd",
+  "July 11th",
+  "July 17th",
+  "September 28th",
+  "November 15th",
 ];
 
 function findFirstJulyBDay(birthdays) {
-    // TODO
+  let julyBirthdays = [];
+  for (const date of birthdays) {
+    if (date.includes("July")) {
+      julyBirthdays.push(date);
+    }
+  }
+  return julyBirthdays[0];
 }
 
 console.log(findFirstJulyBDay(BIRTHDAYS)); // should output "July 11th"
