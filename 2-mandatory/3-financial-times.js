e1c6c5dcafea13766f472c7e5298addbe48ccad7
Lunamzelo <lunamzelo@gmail.com>
2022-08-19 23:16:51 +0100

    updated mandatory

--- 2-mandatory/3-financial-times.js
+++ 2-mandatory/3-financial-times.js
@@ -5,7 +5,14 @@
     Implement the function below, which will return a new array containing only article titles which will fit.
 */
 function potentialHeadlines(allArticleTitles) {
-    // TODO
+  let titles65 = [];
+  for (const title of allArticleTitles) {
+    if (title.length <= 65) {
+      titles65.push(title);
+    }
+  }
+  return titles65;
+  // TODO
 }
 
 /*
@@ -14,7 +21,31 @@ function potentialHeadlines(allArticleTitles) {
     (you can assume words will always be seperated by a space)
 */
 function titleWithFewestWords(allArticleTitles) {
-    // TODO
+    let min = 0;
+    let title;
+    for (let index = 0; index < allArticleTitles.length; index++) {
+      if (min === 0) {
+        min = allArticleTitles[index].split(" ").length;
+        title = allArticleTitles[index];
+      } else {
+          if (min > allArticleTitles[index].split(" ").length) {
+            min = allArticleTitles[index].split(" ").length;
+            title = allArticleTitles[index];
+          }
+        // min =
+        //   min > allArticleTitles[index].split(" ").length
+        //     ? allArticleTitles[index].split(" ").length
+        //     : min;
+        // title = allArticleTitles[index];
+      }
+    }
+    return title;
+    
+  
+
+  
+
+  // TODO
 }
 
 /*
@@ -23,7 +54,16 @@ function titleWithFewestWords(allArticleTitles) {
     (Hint: remember that you can also loop through the characters of a string if you need to)
 */
 function headlinesWithNumbers(allArticleTitles) {
-    // TODO
+  // TODO
+ let headlinesWithNumber= [];
+ for (let index = 0; index < allArticleTitles.length; index++) {
+     if(/\d/.test(allArticleTitles[index])){
+         headlinesWithNumber.push(allArticleTitles[index]);
+     }
+     
+     
+ }
+ return headlinesWithNumber;
 }
 
 /*
@@ -31,51 +71,64 @@ function headlinesWithNumbers(allArticleTitles) {
     Implement the function below to return this number - rounded to the nearest integer.
 */
 function averageNumberOfCharacters(allArticleTitles) {
-    // TODO
+    let average = 0;
+    let sum= 0;
+    for (let index = 0; index < allArticleTitles.length; index++) {
+        sum+= allArticleTitles[index].length;
+        
+    }
+    average = Math.round(sum / allArticleTitles.length);
+    return average;
+    
+  // TODO
 }
-
-
 
 /* ======= List of Articles - DO NOT MODIFY ===== */
 const ARTICLE_TITLES = [
-    "Streaming wars drive media groups to spend more than $100bn on new content",
-    "Amazon Prime Video India country head: streaming is driving a TV revolution",
-    "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
-    "British companies look to muscle in on US retail investing boom",
-    "Libor to take firm step towards oblivion on New Year's Day",
-    "Audit profession unattractive to new recruits, says PwC boss",
-    "Chinese social media users blast Elon Musk over near miss in space",
-    "Companies raise over $12tn in 'blockbuster' year for global capital markets",
-    "The three questions that dominate investment",
-    "Brussels urges Chile's incoming president to endorse EU trade deal",
+  "Streaming wars drive media groups to spend more than $100bn on new content",
+  "Amazon Prime Video India country head: streaming is driving a TV revolution",
+  "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
+  "British companies look to muscle in on US retail investing boom",
+  "Libor to take firm step towards oblivion on New Year's Day",
+  "Audit profession unattractive to new recruits, says PwC boss",
+  "Chinese social media users blast Elon Musk over near miss in space",
+  "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+  "The three questions that dominate investment",
+  "Brussels urges Chile's incoming president to endorse EU trade deal",
 ];
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("should only return potential headlines", () => {
-    expect(new Set(potentialHeadlines(ARTICLE_TITLES))).toEqual(new Set([
-        "British companies look to muscle in on US retail investing boom",
-        "Libor to take firm step towards oblivion on New Year's Day",
-        "Audit profession unattractive to new recruits, says PwC boss",
-        "The three questions that dominate investment"
-    ]));
+  expect(new Set(potentialHeadlines(ARTICLE_TITLES))).toEqual(
+    new Set([
+      "British companies look to muscle in on US retail investing boom",
+      "Libor to take firm step towards oblivion on New Year's Day",
+      "Audit profession unattractive to new recruits, says PwC boss",
+      "The three questions that dominate investment",
+    ])
+  );
 });
 
 test("should return an empty array for empty input", () => {
-    expect(potentialHeadlines([])).toEqual([]);
+  expect(potentialHeadlines([])).toEqual([]);
 });
 
 test("should return the title with the fewest words", () => {
-    expect(titleWithFewestWords(ARTICLE_TITLES)).toEqual("The three questions that dominate investment");
+  expect(titleWithFewestWords(ARTICLE_TITLES)).toEqual(
+    "The three questions that dominate investment"
+  );
 });
 
 test("should only return headlines containing numbers", () => {
-    expect(new Set(headlinesWithNumbers(ARTICLE_TITLES))).toEqual(new Set([
-        "Streaming wars drive media groups to spend more than $100bn on new content",
-        "Companies raise over $12tn in 'blockbuster' year for global capital markets"
-    ]));
+  expect(new Set(headlinesWithNumbers(ARTICLE_TITLES))).toEqual(
+    new Set([
+      "Streaming wars drive media groups to spend more than $100bn on new content",
+      "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+    ])
+  );
 });
 
 test("should return the average number of characters in a headline", () => {
-    expect(averageNumberOfCharacters(ARTICLE_TITLES)).toEqual(65);
+  expect(averageNumberOfCharacters(ARTICLE_TITLES)).toEqual(65);
 });
