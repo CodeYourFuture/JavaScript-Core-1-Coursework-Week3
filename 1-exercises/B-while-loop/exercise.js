44b6400e2b0d195a53a9b86db122eae7bba27d6d
Lunamzelo <lunamzelo@gmail.com>
2022-08-19 23:15:27 +0100

    updated exercise

--- 1-exercises/B-while-loop/exercise.js
+++ 1-exercises/B-while-loop/exercise.js
@@ -6,8 +6,19 @@
 */
 
 function evenNumbers(n) {
-    // TODO
+  let i = 1;
+  let j = 0;
+  let array = [];
+  while (i <= n) {
+    array.push(j);
+    i++;
+    j += 2;
+  }
+  console.log(array.join(","));
+
+  // TODO
 }
+
 
 evenNumbers(3); // should output 0,2,4
 evenNumbers(0); // should output nothing
