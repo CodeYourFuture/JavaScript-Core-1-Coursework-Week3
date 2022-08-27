b0edce133aa564561882c693d1501cb1fcd389dc
GryRgrs <102528828+GryRgrs@users.noreply.github.com>
2022-08-27 18:16:23 +0100

    first attempt at mandatory

--- 2-mandatory/2-retrying-random-numbers.js
+++ 2-mandatory/2-retrying-random-numbers.js
@@ -5,20 +5,30 @@
 
 // This function shouldn't be changed
 function generateRandomNumber() {
-    console.log("Generating number...");
-    return Math.round(Math.random() * 100);
+  console.log("Generating number...");
+  return Math.round(Math.random() * 100);
 }
 
 function getRandomNumberGreaterThan50() {
-    // TODO - implement using a do-while loop
+  let num = Math.floor(Math.random() * upper) + 1;
+  return num;
+  i = 0;
+  do {
+    i += 1;
+    if (i >= 50) {
+      break;
+    }
+    text += i + " ";
+  } while (i <= 50);
 }
+console.log(num);
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("Returned value should always be greater than 50", () => {
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
 });
