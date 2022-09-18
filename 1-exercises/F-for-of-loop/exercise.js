6b34964cc44874278b7581c72bac33a2337ccd9b
Mohammadreza <104798944+Mohammadrezanafar@users.noreply.github.com>
2022-09-18 22:52:35 +0100

    - Used a for-of loop to output an array. - Used a for-of loop to capitalize a string letter by letter.

--- 1-exercises/F-for-of-loop/exercise.js
+++ 1-exercises/F-for-of-loop/exercise.js
@@ -1,5 +1,5 @@
 /*
-    A for-of loop is a easy and way of looping through the elements of an array, string or any other "iterable object" (think sequence of elements).    
+    A for-of loop is an easy way of looping through the elements of an array, string or any other "iterable object" (think sequence of elements).    
 */
 
 // TODO Use a for-of loop to output each of the tube stations below.
@@ -9,8 +9,14 @@ let tubeStations = [
     "Picadilly Circus",
     "Oxford Street",
     "Tottenham Court Road"
-];
-
-
-// TODO Use a for-of loop to capitalise and output each letter in the string seperately.
-let str = "codeyourfuture";
+ ];
+ for (let tubeStation of tubeStations) {
+    console.log(tubeStation);
+    console.log("**********************");
+ }
+ // TODO Use a for-of loop to capitalise and output each letter in the string seperately.
+ let str = "codeyourfuture";
+ 
+ for (let s of str) {
+    console.log(s.toUpperCase());
+ }
