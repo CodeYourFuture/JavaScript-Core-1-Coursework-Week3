a5ebd8e7f4cd4d287f1bff3fa474a1add3b5dd05
Rebwar <anvar.azizi21@gmail.com>
2022-09-19 08:00:08 +0100

    1-exercises__C-while-loop-with-array

--- 1-exercises/C-while-loop-with-array/exercise.js
+++ 1-exercises/C-while-loop-with-array/exercise.js
@@ -13,11 +13,23 @@ const BIRTHDAYS = [
     "July 11th",
     "July 17th",
     "September 28th",
-    "November 15th"
+    "November 15th",
 ];
 
+// let July1 = BIRTHDAYS.filter((x) => x.split(" ")[0] === "July");
+// console.log(July1.sort()[0]);
+
 function findFirstJulyBDay(birthdays) {
-    // TODO
+    let july = [];
+    let count = 0;
+
+    while (count < birthdays.length) {
+        if (birthdays[count].split(" ")[0] === "July") {
+            july.push(birthdays[count]);
+        }
+        count++;
+    }
+    return july.sort()[0];
 }
 
 console.log(findFirstJulyBDay(BIRTHDAYS)); // should output "July 11th"
