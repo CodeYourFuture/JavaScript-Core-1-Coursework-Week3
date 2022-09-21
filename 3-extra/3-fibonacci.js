75b4fa3833460c0fefb2b24780d2d3e9721eef92
leilafaez <79403281+leilafaez@users.noreply.github.com>
2022-09-21 21:15:55 +0100

    Update extra exercise

--- 3-extra/3-fibonacci.js
+++ 3-extra/3-fibonacci.js
@@ -14,7 +14,15 @@
 */
 
 function generateFibonacciSequence(n) {
-    // TODO
+     let fib = [0, 1];
+     let data = [];
+
+     for (let i = 2; i < n; i++) {
+       fib[i] = fib[i - 1] + fib[i - 2];
+       data.push(fib[i]);
+     }
+
+     return fib;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
