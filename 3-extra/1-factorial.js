3929d8b8bb0083ccd62a399197d5dd6d65ad9368
Ekrem <ekremteke.uk@gmail.com>
2022-09-22 00:30:41 +0100

    Mandatory 3 was done

--- 3-extra/1-factorial.js
+++ 3-extra/1-factorial.js
@@ -9,19 +9,19 @@
 */
 
 function factorial(input) {
-    // TODO
+  // TODO
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("3! should be 6", () => {
-    expect(factorial(3)).toEqual(6);
+  expect(factorial(3)).toEqual(6);
 });
 
 test("5! should be 120", () => {
-    expect(factorial(5)).toEqual(120);
+  expect(factorial(5)).toEqual(120);
 });
 
 test("10! should be 3628800", () => {
-    expect(factorial(10)).toEqual(3628800);
+  expect(factorial(10)).toEqual(3628800);
 });
