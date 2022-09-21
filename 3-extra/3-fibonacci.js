810384c2a83c5c376fa94c0f9af3875860562056
Rebwar <anvar.azizi21@gmail.com>
2022-09-21 02:50:06 +0100

    3-extra/3-fibonacci.js

--- 3-extra/3-fibonacci.js
+++ 3-extra/3-fibonacci.js
@@ -14,24 +14,27 @@
 */
 
 function generateFibonacciSequence(n) {
-    // TODO
-}
+    let result = [0, 1];
+    for (let i = 2; i < n; i++) {
+        result[i] = result[i - 2] + result[i - 1];
+    }
 
+    return result;
+}
+// console.log(generateFibonacciSequence(10));
 /* ======= TESTS - DO NOT MODIFY ===== */
 test("should return the first 10 numbers in the Fibonacci Sequence", () => {
-    expect(generateFibonacciSequence(10)).toEqual(
-        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    );
+    expect(generateFibonacciSequence(10)).toEqual([
+        0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
+    ]);
 });
 
 test("should return the first 5 numbers in the Fibonacci Sequence", () => {
-    expect(generateFibonacciSequence(5)).toEqual(
-        [0, 1, 1, 2, 3]
-    );
+    expect(generateFibonacciSequence(5)).toEqual([0, 1, 1, 2, 3]);
 });
 
 test("should return the first 15 numbers in the Fibonacci Sequence", () => {
-    expect(generateFibonacciSequence(15)).toEqual(
-        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
-    );
+    expect(generateFibonacciSequence(15)).toEqual([
+        0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
+    ]);
 });
