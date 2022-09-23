011b4f339ad5db6a1b5b92590ec12287f3e4830f
Zaw Khing <42835438+ZawKhing@users.noreply.github.com>
2022-09-23 06:34:43 +0100

    Complete mandatory 2-retrying-random numbers

--- 2-mandatory/2-retrying-random-numbers.js
+++ 2-mandatory/2-retrying-random-numbers.js
@@ -9,17 +9,18 @@ function generateRandomNumber() {
     return Math.round(Math.random() * 100);
 }
 
-function getRandomNumberGreaterThan50(n) {
+function getRandomNumberGreaterThan50() {
     // TODO - implement using a do-while loop
     let i = 0;
     do{
-        let randomNum = generateRandomNumber();
-        if(randomNum > 50){
-            return randomNum;
-        } 
-     i++;
 
-    }while(n < 51)
+        let randomNum = generateRandomNumber();
+        if (randomNum > 50){
+           return randomNum;
+        }
+       i++; 
+    } while(i < 51)
+
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
