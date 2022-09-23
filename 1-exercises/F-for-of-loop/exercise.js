bb086076923dda397c47e631cb968b1e85983fbf
fatih-celebi <celebifatih.c1@gmail.com>
2022-09-23 03:47:16 +0100

    Complete Exercise-F

--- 1-exercises/F-for-of-loop/exercise.js
+++ 1-exercises/F-for-of-loop/exercise.js
@@ -1,5 +1,7 @@
 /*
-    A for-of loop is a easy and way of looping through the elements of an array, string or any other "iterable object" (think sequence of elements).    
+    A for-of loop is a easy and way of looping through 
+    the elements of an array, string or any other 
+    "iterable object" (think sequence of elements).    
 */
 
 // TODO Use a for-of loop to output each of the tube stations below.
@@ -10,7 +12,12 @@ let tubeStations = [
     "Oxford Street",
     "Tottenham Court Road"
 ];
-
+for (const str of tubeStations) {
+  console.log(str);
+}
 
 // TODO Use a for-of loop to capitalise and output each letter in the string seperately.
 let str = "codeyourfuture";
+for (const iterator of str) {
+    console.log(iterator.toUpperCase());
+}
