82925eecd87f5f00a62f0d9f27da39a9836bfd25
Danti_86 <ajux_86@yahoo.com>
2022-09-29 23:53:12 +0100

    Review Solutions mandatory4

--- 2-mandatory/4-stocks.js
+++ 2-mandatory/4-stocks.js
@@ -35,6 +35,27 @@ const CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS = [
 */
 function getAveragePrices(closingPricesForAllStocks) {
     // TODO
+    let averages = [];
+
+    for(let pricesForStock of closingPricesForAllStocks) {
+        averages.push(getAveragePricesForStock(pricesForStock));
+    }
+
+    return averages;
+}
+
+function getAveragePricesForStock(pricesForStock) {
+    let total = 0;
+
+    for(let price of pricesForStock) {
+        total += price;
+    }
+
+    return roundTo2Decimals(total / pricesForStock.length);
+}
+
+function roundTo2Decimals(num) {
+    return Math.round(num * 100) / 100;
 }
 
 /*
@@ -48,7 +69,18 @@ function getAveragePrices(closingPricesForAllStocks) {
     The price change value should be rounded to 2 decimal places, and should be a number (not a string)
 */
 function getPriceChanges(closingPricesForAllStocks) {
-    // TODO
+    // TODOlet changes = [];
+
+    for(let pricesForStock of closingPricesForAllStocks) {
+        changes.push(getPriceChangeForStock(pricesForStock))
+    }
+
+    return changes;
+}
+
+function getPriceChangeForStock(pricesForStock) {
+    let priceChange = pricesForStock[pricesForStock.length - 1] - pricesForStock[0]
+    return roundTo2Decimals(priceChange);
 }
 
 /*
@@ -65,6 +97,39 @@ function getPriceChanges(closingPricesForAllStocks) {
 */
 function highestPriceDescriptions(closingPricesForAllStocks, stocks) {
     // TODO
+    let descriptions = [];
+
+    for(let i = 0; i < closingPricesForAllStocks.length; i++) {
+        let highestPrice = getHighestPrice(closingPricesForAllStocks[i]);
+        descriptions.push(`The highest price of ${stocks[i].toUpperCase()} in the last 5 days was ${highestPrice.toFixed(2)}`);
+    }
+
+    return descriptions;
+}
+
+function getHighestPrice(pricesForStock) {
+    // initialising to 0, as we're expecting this value to be overriden by the first price in the array
+    let highestPriceSoFar = 0;
+
+    for(let price of pricesForStock) {
+        // if this price is higher than the highest price we've seen so far, it becomes the new highest price
+        if(price > highestPriceSoFar) {
+            highestPriceSoFar = price;
+        }
+    }
+
+    return highestPriceSoFar;
+}
+
+function highestPriceDescriptionsAlternate(closingPricesForAllStocks, stocks) {
+    let descriptions = [];
+
+    for(let i = 0; i < closingPricesForAllStocks.length; i++) {
+        let highestPrice = Math.max(...closingPricesForAllStocks[i]);
+        descriptions.push(`The highest price of ${stocks[i].toUpperCase()} in the last 5 days was ${highestPrice.toFixed(2)}`);
+    }
+
+    return descriptions;
 }
 
 
