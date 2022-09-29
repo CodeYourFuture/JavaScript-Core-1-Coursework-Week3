5d327b7fa99a725d5cec4f7783e87008b861b5b3
Danti_86 <ajux_86@yahoo.com>
2022-09-29 23:54:01 +0100

    Review Solutions extra\3

--- 3-extra/3-fibonacci.js
+++ 3-extra/3-fibonacci.js
@@ -15,6 +15,19 @@
 
 function generateFibonacciSequence(n) {
     // TODO
+    let fibonacciSequence = [];
+
+    // just initialise the first 2 values in the sequence
+    fibonacciSequence[0] = 0;
+    fibonacciSequence[1] = 1;
+
+    // the remaining numbers can be calculated
+    for (i = 2; i < n; i++) {
+        // each number is equal to the sum of the previous 2 numbers
+        fibonacciSequence[i] = fibonacciSequence[i - 2] + fibonacciSequence[i - 1];
+    }
+
+    return fibonacciSequence;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
