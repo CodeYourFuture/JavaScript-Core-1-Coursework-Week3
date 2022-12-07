8071256ab840954aeb861896e819b39f82dea824
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-12-07 19:58:02 +0000

    Exercises - done

--- 1-exercises/E-for-loop/exercise1.js
+++ 1-exercises/E-for-loop/exercise1.js
@@ -4,11 +4,13 @@
     Change the while loop below into a for loop.
 */
 
-
 // Change the below code to use a for loop instead of a while loop.
-let i = 0;
-while(i < 26) {
-    console.log(String.fromCharCode(97 + i));
-    i++;
-}
+// let i = 0;
+// while (i < 26) {
+//   console.log(String.fromCharCode(97 + i));
+//   i++;
+// }
 // The output shouldn't change.
+for (let i = 0; i < 26; i++) {
+  console.log(String.fromCharCode(97 + i));
+}
