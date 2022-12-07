d03c851ab46ef80a021a47cb0e6615dbbc113f49
OleksChep <aachep@gmail.com>
2022-12-07 04:32:48 +0000

    Exercises done

--- 1-exercises/F-for-of-loop/exercise.js
+++ 1-exercises/F-for-of-loop/exercise.js
@@ -10,7 +10,24 @@ let tubeStations = [
     "Oxford Street",
     "Tottenham Court Road"
 ];
+  //const array1 = ["a", "b", "c"];
+
+  for (const element of tubeStations) {
+    console.log(element);
+  }
+
+
+
+
 
 
 // TODO Use a for-of loop to capitalise and output each letter in the string seperately.
 let str = "codeyourfuture";
+let strUpp = str.toUpperCase();
+
+const arr = [strUpp];
+
+for (let element of arr) {
+  console.log(element);
+}
+
