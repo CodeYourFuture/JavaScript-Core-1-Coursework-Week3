9b69442fd1844adb002e80ca30f2176925e83dcf
ys35code <ys35code@gmail.com>
2022-12-08 23:04:17 +0000

    extra

--- 3-extra/3-fibonacci.js
+++ 3-extra/3-fibonacci.js
@@ -14,24 +14,33 @@
 */
 
 function generateFibonacciSequence(n) {
-    // TODO
+  let arrf = [];
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    if (arrf.length >= 2) {
+      sum = arrf[i - 1] + arrf[i - 2];
+      arrf.push(sum);
+    } else {
+      sum += i;
+      arrf.push(sum);
+    }
+  }
+  return arrf;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 test("should return the first 10 numbers in the Fibonacci Sequence", () => {
-    expect(generateFibonacciSequence(10)).toEqual(
-        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    );
+  expect(generateFibonacciSequence(10)).toEqual([
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
+  ]);
 });
 
 test("should return the first 5 numbers in the Fibonacci Sequence", () => {
-    expect(generateFibonacciSequence(5)).toEqual(
-        [0, 1, 1, 2, 3]
-    );
+  expect(generateFibonacciSequence(5)).toEqual([0, 1, 1, 2, 3]);
 });
 
 test("should return the first 15 numbers in the Fibonacci Sequence", () => {
-    expect(generateFibonacciSequence(15)).toEqual(
-        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
-    );
+  expect(generateFibonacciSequence(15)).toEqual([
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
+  ]);
 });
