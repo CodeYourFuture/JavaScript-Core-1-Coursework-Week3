f1ab4588fa6ba1e56c62eeb4706f3652c6e68a24
SagharHosseinmardi <saghar.h.mardi@gmail.com>
2022-12-10 00:01:38 +0000

    mandatory 1

--- 2-mandatory/2-retrying-random-numbers.js
+++ 2-mandatory/2-retrying-random-numbers.js
@@ -5,20 +5,20 @@
 
 // This function shouldn't be changed
 function generateRandomNumber() {
-    console.log("Generating number...");
-    return Math.round(Math.random() * 100);
+  console.log("Generating number...");
+  return Math.round(Math.random() * 100);
 }
 
 function getRandomNumberGreaterThan50() {
-    // TODO - implement using a do-while loop
+  // TODO - implement using a do-while loop
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("Returned value should always be greater than 50", () => {
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
-    expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
+  expect(getRandomNumberGreaterThan50()).toBeGreaterThan(50);
 });
