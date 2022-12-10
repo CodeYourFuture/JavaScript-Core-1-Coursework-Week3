0e425e2de59c398bfcc588836efff5e2804c5245
Bahare <itxbb09@gmail.com>
2022-12-10 12:45:21 +0000

    update extra-2

--- 3-extra/2-array-of-objects.js
+++ 3-extra/2-array-of-objects.js
@@ -11,75 +11,97 @@
 */
 
 function getHighestRatedInEachGenre(books) {
-    // TODO
-    for(let book of books){
-        
+  // TODO
+  let child = [];
+  let scFiction = [];
+  let cook = [];
+  resultArr = [];
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].genre === "children") {
+      child.push(books[i]);
+    } else if (books[i].genre === "cooking") {
+      cook.push(books[i]);
+    } else {
+      // console.log("helllo");
+      scFiction.push(books[i]);
     }
-}
+  }
+  console.log(scFiction, child, cook);
 
+  let scFictionSorted = scFiction.sort((a, b) => a.rating - b.rating);
+  let childSorted = child.sort((a, b) => a.rating - b.rating);
+
+  let cookSorted = cook.sort((a, b) => a.rating - b.rating);
+
+  resultArr.push(scFictionSorted[scFictionSorted.length - 1].title);
+  resultArr.push(childSorted[childSorted.length - 1].title);
+  resultArr.push(cookSorted[cookSorted.length - 1].title);
+
+  return resultArr;
+}
 
 /* ======= Book data - DO NOT MODIFY ===== */
 const BOOKS = [
-    {
-        title: "The Lion, the Witch and the Wardrobe",
-        genre: "children",
-        rating: 4.7
-    },
-    {
-        title: "Sapiens: A Brief History of Humankind",
-        genre: "non-fiction",
-        rating: 4.7
-    },
-    {
-        title: "Nadiya's Fast Flavours",
-        genre: "cooking",
-        rating: 4.7
-    },
-    {
-        title: "Harry Potter and the Philosopher's Stone",
-        genre: "children",
-        rating: 4.8
-    },
-    {
-        title: "A Life on Our Planet",
-        genre: "non-fiction",
-        rating: 4.8
-    },
-    {
-        title: "Dishoom: The first ever cookbook from the much-loved Indian restaurant",
-        genre: "cooking",
-        rating: 4.85
-    },
-    {
-        title: "Gangsta Granny Strikes Again!",
-        genre: "children",
-        rating: 4.9
-    },
-    {
-        title: "Diary of a Wimpy Kid",
-        genre: "children",
-        rating: 4.6
-    },
-    {
-        title: "BOSH!: Simple recipes. Unbelievable results. All plants.",
-        genre: "cooking",
-        rating: 4.6
-    },
-    {
-        title: "The Book Your Dog Wishes You Would Read",
-        genre: "non-fiction",
-        rating: 4.85
-    },
-]
-
+  {
+    title: "The Lion, the Witch and the Wardrobe",
+    genre: "children",
+    rating: 4.7,
+  },
+  {
+    title: "Sapiens: A Brief History of Humankind",
+    genre: "non-fiction",
+    rating: 4.7,
+  },
+  {
+    title: "Nadiya's Fast Flavours",
+    genre: "cooking",
+    rating: 4.7,
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    genre: "children",
+    rating: 4.8,
+  },
+  {
+    title: "A Life on Our Planet",
+    genre: "non-fiction",
+    rating: 4.8,
+  },
+  {
+    title:
+      "Dishoom: The first ever cookbook from the much-loved Indian restaurant",
+    genre: "cooking",
+    rating: 4.85,
+  },
+  {
+    title: "Gangsta Granny Strikes Again!",
+    genre: "children",
+    rating: 4.9,
+  },
+  {
+    title: "Diary of a Wimpy Kid",
+    genre: "children",
+    rating: 4.6,
+  },
+  {
+    title: "BOSH!: Simple recipes. Unbelievable results. All plants.",
+    genre: "cooking",
+    rating: 4.6,
+  },
+  {
+    title: "The Book Your Dog Wishes You Would Read",
+    genre: "non-fiction",
+    rating: 4.85,
+  },
+];
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 test("should return the highest rated book in each genre", () => {
-    expect(new Set(getHighestRatedInEachGenre(BOOKS))).toEqual(new Set(
-        [
-            "The Book Your Dog Wishes You Would Read",
-            "Gangsta Granny Strikes Again!",
-            "Dishoom: The first ever cookbook from the much-loved Indian restaurant"
-        ]
-    ));
+  expect(new Set(getHighestRatedInEachGenre(BOOKS))).toEqual(
+    new Set([
+      "The Book Your Dog Wishes You Would Read",
+      "Gangsta Granny Strikes Again!",
+      "Dishoom: The first ever cookbook from the much-loved Indian restaurant",
+    ])
+  );
 });
