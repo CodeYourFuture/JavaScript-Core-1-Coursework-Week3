a9a1d6735d89594e118de9cbe95b141b8af65bae
Pamelalun <105378916+Pamelalun@users.noreply.github.com>
2023-01-05 15:55:46 +0000

    jsExerciseB
    completed

--- 1-exercises/B-while-loop/exercise.js
+++ 1-exercises/B-while-loop/exercise.js
@@ -3,12 +3,25 @@
 
     Using a while loop, complete the function below so it logs (using console.log) the first n even numbers as a comma-seperated string.
     The list of numbers should start with 0. n is being passed in as a parameter.
-*/
+// */
 
 function evenNumbers(n) {
     // TODO
+    let num = 0;
+    let even = [];
+    while (n > 0) {
+      arr.push(num);
+      num = num + 2;
+      n--;
+    }
+  
+    console.log(even);
 }
 
 evenNumbers(3); // should output 0,2,4
 evenNumbers(0); // should output nothing
 evenNumbers(10); // should output 0,2,4,6,8,10,12,14,16,18
+
+
+
+
