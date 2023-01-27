df7d0dec4813a4797d75bbc550b3532e666f8914
dubchoi <will@jomolabs.co>
2023-01-27 10:21:12 -0800

    Swap exercises with week2 to match the syllabus.

---
+++ 1-exercises/E-while-loop-with-array/exercise.js
@@ -0,0 +1,23 @@
+/*
+    Loops can be useful when working with arrays.
+    In the below example, imagine we've defined an array holding the birthdays of your closest friends.
+    Use a while loop to search through the array until you find the first birthday in July, then return that birthday from the function.
+*/
+
+const BIRTHDAYS = [
+    "January 7th",
+    "February 12th",
+    "April 3rd",
+    "April 5th",
+    "May 3rd",
+    "July 11th",
+    "July 17th",
+    "September 28th",
+    "November 15th"
+];
+
+function findFirstJulyBDay(birthdays) {
+    // TODO
+}
+
+console.log(findFirstJulyBDay(BIRTHDAYS)); // should output "July 11th"
