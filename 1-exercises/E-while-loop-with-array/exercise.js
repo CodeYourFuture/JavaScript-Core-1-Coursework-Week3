e4fdc888f96fb90b6c85ce116c5840224eca209f
malkitbenning <malkitbenning@gmail.com>
2023-03-02 10:44:21 +0000

    Initial Exercises Complete

--- 1-exercises/E-while-loop-with-array/exercise.js
+++ 1-exercises/E-while-loop-with-array/exercise.js
@@ -5,19 +5,30 @@
 */
 
 const BIRTHDAYS = [
-    "January 7th",
-    "February 12th",
-    "April 3rd",
-    "April 5th",
-    "May 3rd",
-    "July 11th",
-    "July 17th",
-    "September 28th",
-    "November 15th"
+  "January 7th",
+  "February 12th",
+  "April 3rd",
+  "April 5th",
+  "May 3rd",
+  "July 11th",
+  "July 17th",
+  "September 28th",
+  "November 15th",
 ];
 
 function findFirstJulyBDay(birthdays) {
-    // TODO
+  // TODO
+  //   for (let i = 0; i < BIRTHDAYS.length; i++) {
+  //     if (BIRTHDAYS[i].includes("July")) {
+  //       return BIRTHDAYS[i];
+  //     }
+  //   }
+
+  for (let birthday of BIRTHDAYS) {
+    if (birthday.includes("July")) {
+      return birthday;
+    }
+  }
 }
 
 console.log(findFirstJulyBDay(BIRTHDAYS)); // should output "July 11th"
