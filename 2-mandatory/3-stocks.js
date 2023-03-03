597fafc2e700a971f1e0772c00878bc393381e93
Appolin Semegni Fotso <108607571+AppolinFotso@users.noreply.github.com>
2023-03-03 10:53:07 +0000

    Passed the test

--- 2-mandatory/3-stocks.js
+++ 2-mandatory/3-stocks.js
@@ -11,11 +11,11 @@
 const STOCKS = ["aapl", "msft", "amzn", "googl", "tsla"];
 
 const CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS = [
-    [179.19, 180.33, 176.28, 175.64, 172.99], // AAPL
-    [340.69, 342.45, 334.69, 333.20, 327.29], // MSFT
-    [3384.44, 3393.39, 3421.37, 3420.74, 3408.34], // AMZN
-    [2951.88, 2958.13, 2938.33, 2928.30, 2869.45], // GOOGL
-    [1101.30, 1093.94, 1067.00, 1008.87, 938.53] // TSLA
+  [179.19, 180.33, 176.28, 175.64, 172.99], // AAPL
+  [340.69, 342.45, 334.69, 333.2, 327.29], // MSFT
+  [3384.44, 3393.39, 3421.37, 3420.74, 3408.34], // AMZN
+  [2951.88, 2958.13, 2938.33, 2928.3, 2869.45], // GOOGL
+  [1101.3, 1093.94, 1067.0, 1008.87, 938.53], // TSLA
 ];
 
 /*
@@ -34,7 +34,18 @@ const CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS = [
         Functions can help with this!
 */
 function getAveragePrices(closingPricesForAllStocks) {
-    // TODO
+  // TODO
+  const averagePrice = [];
+  for (let arr of closingPricesForAllStocks) {
+    let x = 0;
+    for (let i = 0; i < arr.length; i++) {
+      x += arr[i];
+    }
+    let pushValue = Number((x / arr.length).toFixed(2));
+    averagePrice.push(pushValue);
+  }
+
+  return averagePrice;
 }
 
 /*
@@ -48,7 +59,13 @@ function getAveragePrices(closingPricesForAllStocks) {
     The price change value should be rounded to 2 decimal places, and should be a number (not a string)
 */
 function getPriceChanges(closingPricesForAllStocks) {
-    // TODO
+  // TODO
+  const priceChange = [];
+  for (let arr of closingPricesForAllStocks) {
+    priceChange.push(Number((arr[arr.length - 1] - arr[0]).toFixed(2)));
+  }
+
+  return priceChange;
 }
 
 /*
@@ -64,31 +81,43 @@ function getPriceChanges(closingPricesForAllStocks) {
     The price should be shown with exactly 2 decimal places.
 */
 function highestPriceDescriptions(closingPricesForAllStocks, stocks) {
-    // TODO
-}
+  // TODO
+  const highestPrice = [];
+  for (let arr of closingPricesForAllStocks) {
+    let x = 0;
+    let y = 0;
+    x = Math.max(...arr).toFixed(2);
+    y = `The highest price of ${stocks[
+      closingPricesForAllStocks.indexOf(arr)
+    ].toUpperCase()} in the last 5 days was ${x}`;
 
+    highestPrice.push(y);
+  }
+
+  return highestPrice;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 test("should return the average price for each stock", () => {
-    expect(getAveragePrices(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual(
-        [176.89, 335.66, 3405.66, 2929.22, 1041.93]
-    );
+  expect(getAveragePrices(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual([
+    176.89, 335.66, 3405.66, 2929.22, 1041.93,
+  ]);
 });
 
 test("should return the price change for each stock", () => {
-    expect(getPriceChanges(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual(
-        [-6.2, -13.4, 23.9, -82.43, -162.77]
-    );
+  expect(getPriceChanges(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS)).toEqual([
+    -6.2, -13.4, 23.9, -82.43, -162.77,
+  ]);
 });
 
 test("should return a description of the highest price for each stock", () => {
-    expect(highestPriceDescriptions(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS, STOCKS)).toEqual(
-        [
-            "The highest price of AAPL in the last 5 days was 180.33",
-            "The highest price of MSFT in the last 5 days was 342.45",
-            "The highest price of AMZN in the last 5 days was 3421.37",
-            "The highest price of GOOGL in the last 5 days was 2958.13",
-            "The highest price of TSLA in the last 5 days was 1101.30"
-        ]
-    );
+  expect(
+    highestPriceDescriptions(CLOSING_PRICES_LAST_5_DAYS_FOR_ALL_STOCKS, STOCKS)
+  ).toEqual([
+    "The highest price of AAPL in the last 5 days was 180.33",
+    "The highest price of MSFT in the last 5 days was 342.45",
+    "The highest price of AMZN in the last 5 days was 3421.37",
+    "The highest price of GOOGL in the last 5 days was 2958.13",
+    "The highest price of TSLA in the last 5 days was 1101.30",
+  ]);
 });
