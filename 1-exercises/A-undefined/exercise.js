9eb09a34d0ffebc1b3002c357359ca3f33e2dfa9
fowobi <109865505+fowobi@users.noreply.github.com>
2023-03-06 12:40:21 +0000

    Answer the Undefined exercise
    Answer the Undefined exercise

--- 1-exercises/A-undefined/exercise.js
+++ 1-exercises/A-undefined/exercise.js
@@ -13,6 +13,10 @@
 let a;
 console.log(a);
 
+// Answer: Declaring a variblewith 'let' but not assigning it a 
+//value makes the varible have a default value of 'undefined'
+//then the 'console.log' will print 'undefined'
+
 
 // Example 2
 function sayHello() {
@@ -22,6 +26,9 @@ function sayHello() {
 let hello = sayHello();
 console.log(hello);
 
+// Answer: The function 'sayHello()' does not retun anything,and when you assign the 
+//result of 'sayHello()' to the variable 'hello','hello' will be undefined.
+
 
 // Example 3
 function sayHelloToUser(user) {
@@ -30,7 +37,14 @@ function sayHelloToUser(user) {
 
 sayHelloToUser();
 
+//Answer: The function 'sayHelloToUser()' expects an argument 'user'
+// to be passed when it is called but when it is called without any 
+//argument ,'user' is 'undefined',this resulting in the string template to be 
+//'Hello Undefined'.
+
 
 // Example 4
 let arr = [1,2,3];
 console.log(arr[3]);
+
+//Answer: arr[3] is undefined because the array arr only has elements indexed from 0 to 2, and there is no element at index 3.
