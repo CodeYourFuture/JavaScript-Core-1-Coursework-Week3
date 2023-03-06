eb4121fdd61cc79de5b698a167e51fd2dd21b67b
fowobi <109865505+fowobi@users.noreply.github.com>
2023-03-06 12:16:03 +0000

    Extra question Answered
    Extra Answered

--- 3-extra/3-fibonacci.js
+++ 3-extra/3-fibonacci.js
@@ -15,6 +15,13 @@
 
 function generateFibonacciSequence(n) {
     // TODO
+    const sequence = [0, 1];
+  
+  for (let i = 2; i < n; i++) {
+    sequence[i] = sequence[i-1] + sequence[i-2];
+  }
+
+  return sequence;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
