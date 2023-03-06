5da1be33804125e9d894f34ede322657ec86a199
Hussein <hussein.codes@gmail.com>
2023-03-06 00:49:39 +0000

    mand3 task 3 1st attempt

--- 2-mandatory/3-stocks.js
+++ 2-mandatory/3-stocks.js
@@ -82,7 +82,22 @@ function getPriceChanges(closingPricesForAllStocks) {
     The price should be shown with exactly 2 decimal places.
 */
 function highestPriceDescriptions(closingPricesForAllStocks, stocks) {
-    // TODO
+    const highestPricereports = [];
+
+    for (const stock of stocks){
+        let highestPrice = 0;
+
+        for (const price of stock){ 
+
+            if (price > 0){
+                highestPrice = price;
+            }
+        }
+        highestPrice = Number(highestPrice.toFixed(2));
+        highestPricereports.push(`The highest price of ${toUpperCase(stock)} in the last 5 days was ${highestPrice}`);
+    }
+
+    return highestPricereports;
 }
 
 
