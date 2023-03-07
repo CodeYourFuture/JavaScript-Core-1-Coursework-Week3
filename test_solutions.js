2b28825d9ba1148f75d6813dc488b37623ea8633
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-03-07 11:41:31 +0000

    mandatory 2nd

---
+++ test_solutions.js
@@ -0,0 +1,24 @@
+function potentialHeadlines(allArticleTitles) {
+  let returnedArray = [];
+  for (let i = 0; i < allArticleTitles.length; i++) {
+    if (allArticleTitles[i].length <= 65) {
+      returnedArray.push(allArticleTitles[i]);
+    }
+  }
+  return returnedArray;
+}
+
+const ARTICLE_TITLES = [
+  "Streaming wars drive media groups to spend more than $100bn on new content",
+  "Amazon Prime Video India country head: streaming is driving a TV revolution",
+  "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
+  "British companies look to muscle in on US retail investing boom",
+  "Libor to take firm step towards oblivion on New Year's Day",
+  "Audit profession unattractive to new recruits, says PwC boss",
+  "Chinese social media users blast Elon Musk over near miss in space",
+  "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+  "The three questions that dominate investment",
+  "Brussels urges Chile's incoming president to endorse EU trade deal",
+];
+
+console.log(potentialHeadlines(ARTICLE_TITLES));
