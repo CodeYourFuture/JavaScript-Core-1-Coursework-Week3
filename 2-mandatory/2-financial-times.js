a8ac96d870698d8ee82e17897a4540862e3867b2
Douglas Greenshields <dgreenshields@gmail.com>
2023-03-09 19:27:55 +0000

    isolate tests in exercise

--- 2-mandatory/2-financial-times.js
+++ 2-mandatory/2-financial-times.js
@@ -37,23 +37,25 @@ function averageNumberOfCharacters(allArticleTitles) {
 
 
 /* ======= List of Articles - DO NOT MODIFY ===== */
-const ARTICLE_TITLES = [
-    "Streaming wars drive media groups to spend more than $100bn on new content",
-    "Amazon Prime Video India country head: streaming is driving a TV revolution",
-    "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
-    "British companies look to muscle in on US retail investing boom",
-    "Libor to take firm step towards oblivion on New Year's Day",
-    "Audit profession unattractive to new recruits, says PwC boss",
-    "Chinese social media users blast Elon Musk over near miss in space",
-    "Companies raise over $12tn in 'blockbuster' year for global capital markets",
-    "The three questions that dominate investment",
-    "Brussels urges Chile's incoming president to endorse EU trade deal",
-];
+function articleTitles() {
+    return [
+        "Streaming wars drive media groups to spend more than $100bn on new content",
+        "Amazon Prime Video India country head: streaming is driving a TV revolution",
+        "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
+        "British companies look to muscle in on US retail investing boom",
+        "Libor to take firm step towards oblivion on New Year's Day",
+        "Audit profession unattractive to new recruits, says PwC boss",
+        "Chinese social media users blast Elon Musk over near miss in space",
+        "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+        "The three questions that dominate investment",
+        "Brussels urges Chile's incoming president to endorse EU trade deal",
+    ];
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("should only return potential headlines", () => {
-    expect(new Set(potentialHeadlines(ARTICLE_TITLES))).toEqual(new Set([
+    expect(new Set(potentialHeadlines(articleTitles()))).toEqual(new Set([
         "British companies look to muscle in on US retail investing boom",
         "Libor to take firm step towards oblivion on New Year's Day",
         "Audit profession unattractive to new recruits, says PwC boss",
@@ -66,16 +68,16 @@ test("should return an empty array for empty input", () => {
 });
 
 test("should return the title with the fewest words", () => {
-    expect(titleWithFewestWords(ARTICLE_TITLES)).toEqual("The three questions that dominate investment");
+    expect(titleWithFewestWords(articleTitles())).toEqual("The three questions that dominate investment");
 });
 
 test("should only return headlines containing numbers", () => {
-    expect(new Set(headlinesWithNumbers(ARTICLE_TITLES))).toEqual(new Set([
+    expect(new Set(headlinesWithNumbers(articleTitles()))).toEqual(new Set([
         "Streaming wars drive media groups to spend more than $100bn on new content",
         "Companies raise over $12tn in 'blockbuster' year for global capital markets"
     ]));
 });
 
 test("should return the average number of characters in a headline", () => {
-    expect(averageNumberOfCharacters(ARTICLE_TITLES)).toEqual(65);
+    expect(averageNumberOfCharacters(articleTitles())).toEqual(65);
 });
