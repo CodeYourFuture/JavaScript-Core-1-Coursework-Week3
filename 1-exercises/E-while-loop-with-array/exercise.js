e232be24da06e6b923c138865a352076beabb462
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-03-13 16:41:04 +0000

    exercises
    completed exercises

--- 1-exercises/E-while-loop-with-array/exercise.js
+++ 1-exercises/E-while-loop-with-array/exercise.js
@@ -5,19 +5,26 @@
 */
 
 const BIRTHDAYS = [
-    "January 7th",
-    "February 12th",
-    "April 3rd",
-    "April 5th",
-    "May 3rd",
-    "July 11th",
-    "July 17th",
-    "September 28th",
-    "November 15th"
+  "January 7th",
+  "February 12th",
+  "April 3rd",
+  "April 5th",
+  "May 3rd",
+  "July 11th",
+  "July 17th",
+  "September 28th",
+  "November 15th",
 ];
 
 function findFirstJulyBDay(birthdays) {
-    // TODO
+  // TODO
+  let i = 0;
+  while (i < birthdays.lenght) {
+    if (birthdays[i].startsWith("July")) {
+      return BIRTHDAYS[i];
+    }
+    i++;
+  }
 }
 
 console.log(findFirstJulyBDay(BIRTHDAYS)); // should output "July 11th"
