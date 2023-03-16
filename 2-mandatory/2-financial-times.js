9d4e4c05ee4bf0248ab240607a4c6565e23d6289
Saliha Popal <salihapopal@gmail.com>
2023-03-16 10:13:24 +0000

    Adding filter method

--- 2-mandatory/2-financial-times.js
+++ 2-mandatory/2-financial-times.js
@@ -5,42 +5,97 @@
     Implement the function below, which will return a new array containing only article titles which will fit.
 */
 
-    function potentialHeadlines(allArticleTitles) {
-        // Create an empty array to store the potential headlines
-        const potentialHeadlines = [];
+    // function potentialHeadlines(allArticleTitles) {
+    //     // Create an empty array to store the potential headlines
+    //     const potentialHeadlines = [];
       
-        // Loop through each article title in the input array
-        for (let i = 0; i < allArticleTitles.length; i++) {
-          const articleTitle = allArticleTitles[i];
+    //     // Loop through each article title in the input array
+    //     for (let i = 0; i < allArticleTitles.length; i++) {
+    //       const articleTitle = allArticleTitles[i];
       
-          // Check if the title is 65 characters or less
-          if (articleTitle.length <= 65) {
-            // If it is, add it to the potential headlines array
-            potentialHeadlines.push(articleTitle);
-          }
-        }
+    //       // Check if the title is 65 characters or less
+    //       if (articleTitle.length <= 65) {
+    //         // If it is, add it to the potential headlines array
+    //         potentialHeadlines.push(articleTitle);
+    //       }
+    //     }
       
-        // Return the array of potential headlines
-        return potentialHeadlines;
-      }
-      
-/*
+    //     // Return the array of potential headlines
+    //     return potentialHeadlines;
+    //   }
+
+    
+    //   Using filter function with loops
+
+    // function filterPotentialHeadlines(allArticleTitles){
+    //   let potentialHeadlines = [];
+    //   for(let title of allArticleTitles){
+    //     if(title.length <= 65){
+    //       potentialHeadlines.push(title);
+    //     }
+    //   }
+    //   return potentialHeadlines;
+    // }
+
+    // const allArticleTitles = [   "Streaming wars drive media groups to spend more than $100bn on new content",
+    // "Amazon Prime Video India country head: streaming is driving a TV revolution",
+    // "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
+    // "British companies look to muscle in on US retail investing boom",
+    // "Labor to take firm step towards oblivion on New Year's Day",
+    // "Audit profession unattractive to new recruits, says PwC boss",
+    // "Chinese social media users blast Elan Musk over near miss in space",
+    // "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+    // "The three questions that dominate investment",
+    // "Brussels urges Chile's incoming president to endorse EU trade deal",]
+    //   console.log(filterPotentialHeadlines(allArticleTitles));
+    
+
+    //   Using the filter function without loops
+
+   
+    function isPotentialHeadline(articleTitle){
+      return articleTitle.length <= 65;
+    }
+
+    function filterPotentialHeadlines(articleTitles){ 
+      // Here filter takes the function as a parameter
+      return articleTitles.filter(isPotentialHeadline);    
+    }
+
+      const articleTitles = [  "Streaming wars drive media groups to spend more than $100bn on new content",
+      "Amazon Prime Video India country head: streaming is driving a TV revolution",
+      "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
+      "British companies look to muscle in on US retail investing boom",
+      "Labor to take firm step towards oblivion on New Year's Day",
+      "Audit profession unattractive to new recruits, says PwC boss",
+      "Chinese social media users blast Elan Musk over near miss in space",
+      "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+      "The three questions that dominate investment",
+      "Brussels urges Chile's incoming president to endorse EU trade deal",]
+
+      console.log(filterPotentialHeadlines(articleTitles));
+    
+   
+    
+
+
+/* 
     The editor of the FT likes short headlines with only a few words!
     Implement the function below, which returns the title with the fewest words.
     (you can assume words will always be separated by a space)
 */
-function titleWithFewestWords(allArticleTitles) {
-   
-    let fewestWords = allArticleTitles[0];
-  
-    for (let i = 0; i < allArticleTitles.length; i++) {
-      if (fewestWords.length > allArticleTitles[i].length) {
-            fewestWords = allArticleTitles[i];
-      }
+          function titleWithFewestWords(allArticleTitles) {
+
+  let fewestWords = allArticleTitles[0];
+
+  for (let i = 0; i < allArticleTitles.length; i++) {
+    if (fewestWords.length > allArticleTitles[i].length) {
+      fewestWords = allArticleTitles[i];
     }
-  
-    return fewestWords;
   }
+
+  return fewestWords;
+}
   
 
 /*
@@ -107,7 +162,7 @@ const ARTICLE_TITLES = [
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("should only return potential headlines", () => {
-    expect(new Set(potentialHeadlines(ARTICLE_TITLES))).toEqual(new Set([
+    expect(new Set(filterPotentialHeadlines(ARTICLE_TITLES))).toEqual(new Set([
         "British companies look to muscle in on US retail investing boom",
         "Labor to take firm step towards oblivion on New Year's Day",
         "Audit profession unattractive to new recruits, says PwC boss",
@@ -116,7 +171,7 @@ test("should only return potential headlines", () => {
 });
 
 test("should return an empty array for empty input", () => {
-    expect(potentialHeadlines([])).toEqual([]);
+    expect(filterPotentialHeadlines([])).toEqual([]);
 });
 
 test("should return the title with the fewest words", () => {
